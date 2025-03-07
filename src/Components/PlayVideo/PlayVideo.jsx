76cbc6ee466b7e97136a31bf0f66d87074ee5d2a
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2025-03-07 16:22:19 +0530

    6th march

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,19 +1,47 @@
 import "./PlayVideo.css";
-import video1 from "../../assets/video.mp4";
+
 import like from "../../assets/like.png";
 import dislike from "../../assets/dislike.png";
 import share from "../../assets/share.png";
 import save from "../../assets/save.png";
 import jack from "../../assets/jack.png";
 import user_profile from "../../assets/user_profile.jpg";
+import { useState, useEffect } from "react";
+import { API_KEY, value_converter } from "../../data";
 
-const PlayVideo = () => {
+import moment from "moment";
+
+const PlayVideo = ({ videoId }) => {
+  const [apiData, setApiData] = useState(null);
+
+  const fetchVideoData = async () => {
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+    await fetch(videoDetails_url)
+      .then((res) => res.json())
+      .then((data) => setApiData(data.items[0]));
+  };
+
+  useEffect(() => {
+    fetchVideoData();
+  }, []);
+
   return (
     <div className="play-video">
-      <video src={video1} controls autoPlay muted></video>
-      <h3>Best Youtube Channel To Learn Web Development</h3>
+      {/* <video src={video1} controls autoPlay muted></video> */}
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <h3>{apiData ? apiData.snippet.title : "Title here"}</h3>
       <div className="play-video-info">
-        <p>1525 Views &bull; 2 days ago </p>
+        <p>
+          {apiData ? value_converter(apiData.statistics.viewCount) : "16k"}{" "}
+          Views &bull;{" "}
+          {apiData ? moment(apiData.snippet.publishedAt).fromNow() : ""}
+        </p>
         <div>
           <span>
             <img src={like} alt="" />
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
+import { useParams } from "react-router-dom";
 import PlayVideo from "../../Components/PlayVideo/PlayVideo";
 import Recommended from "../../Components/Recommended/Recommended";
 import "./Video.css";
 const Video = () => {
+  const { videoId, categoryId } = useParams();
   return (
     <div className="play-container">
-      <PlayVideo />
+      <PlayVideo videoId={videoId} />
       <Recommended />
     </div>
   );
